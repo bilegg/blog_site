bcf661beaad9b83025334d1fbecba79575ebcff6
bilegg <waresiakp@gmail.com>
2020-08-21 19:14:18 +0200

    Dynamic titles

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', {title: 'Home'})
 })
 
 app.get('/about', (req, res) => {
-	res.render('about')
+	res.render('about', {title: 'About'})
 })
 
 app.get('/blogs/create', (req, res) => {
-	res.render('create')
+	res.render('create', {title: 'Create a new blog'})
+})
+
+app.use((req, res) => {
+	res.render('404', {title: 'Error'})
 })
