b7442a0b11ab3aa78f5d3ce3f4e61f0c99da4d47
bilegg <waresiakp@gmail.com>
2020-08-21 19:39:30 +0200

    Added partials

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
-	console.log('server starting at port 3000')
-})
+    console.log("server starting at port 3000");
+});
 
-app.get('/', (req, res) => {
-	res.render('index', {title: 'Home'})
-})
+app.get("/", (req, res) => {
+    const blogs = [
+        {
+            title: "How to make chicken sandwich",
+            snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "My journey in a new country",
+            snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Getting a new car",
+            snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+    ];
+    res.render("index", {
+        title: "Home",
+        blogs,
+    });
+});
 
-app.get('/about', (req, res) => {
-	res.render('about', {title: 'About'})
-})
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About",
+    });
+});
 
-app.get('/blogs/create', (req, res) => {
-	res.render('create', {title: 'Create a new blog'})
-})
+app.get("/blogs/create", (req, res) => {
+    res.render("create", {
+        title: "Create a new blog",
+    });
+});
 
 app.use((req, res) => {
-	res.render('404', {title: 'Error'})
-})
+    res.render("404", {
+        title: "Error",
+    });
+});
